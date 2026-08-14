c553c1c09069ade8bb63397fe62be41cef9348f1
kaynt <147910852+trungtaottn@users.noreply.github.com>
2026-08-14 16:01:41 +0700

    ci: add semantic release automation (#51)

---
+++ release.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  branches: [
+    { name: "main" },
+    { name: "dev", prerelease: "beta" },
+  ],
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
